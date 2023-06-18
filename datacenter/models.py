1011c221a7827655045d4e84b5c11686858a22bc
Pavel <pas-zhukov@yandex.ru>
2023-06-18 18:28:23 +0300

    finishing all the assignments

--- datacenter/models.py
+++ datacenter/models.py
@@ -1,7 +1,20 @@
 from django.db import models
+from django.utils.timezone import localtime, now
 
 
-class Passcard(models.Model):
+class DictView:
+    """
+    Класс для отображения атрибутов в виде словаря, необходим для отладки.
+    """
+    @property
+    def dict(self):
+        dictionary = self.__dict__
+        dictionary.pop("_state")
+        dictionary.pop("id")
+        return dictionary
+
+
+class Passcard(models.Model, DictView):
     is_active = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now=True)
     passcode = models.CharField(max_length=200, unique=True)
@@ -13,7 +26,7 @@ class Passcard(models.Model):
         return f'{self.owner_name} (inactive)'
 
 
-class Visit(models.Model):
+class Visit(models.Model, DictView):
     created_at = models.DateTimeField(auto_now=True)
     passcard = models.ForeignKey(Passcard, on_delete=models.CASCADE)
     entered_at = models.DateTimeField()
@@ -28,3 +41,21 @@ class Visit(models.Model):
                 if self.leaved_at else 'not leaved'
             )
         )
+
+    @property
+    def duration(self):
+        if self.leaved_at:
+            time_to_count_duration = self.leaved_at
+        else:
+            time_to_count_duration = localtime(now().replace(microsecond=0),
+                                               self.entered_at.tzinfo)
+        duration = time_to_count_duration - self.entered_at
+        return duration
+
+    def formatted_duration(self) -> str:
+        hours = int(self.duration.total_seconds() // 3600)
+        minutes = int(self.duration.total_seconds() % 3600 // 60)
+        return f"{hours}ч {minutes}мин"
+
+    def is_long(self, minutes: int = 60) -> bool:
+        return self.duration.total_seconds() / 60 > minutes
--- datacenter/storage_information_view.py
+++ datacenter/storage_information_view.py
@@ -1,19 +1,22 @@
-from datacenter.models import Passcard
 from datacenter.models import Visit
 from django.shortcuts import render
+from django.utils.timezone import localtime, get_default_timezone
 
 
 def storage_information_view(request):
-    # Программируем здесь
+    unclosed_visits = Visit.objects.filter(leaved_at=None)
 
-    non_closed_visits = [
-        {
-            'who_entered': 'Richard Shaw',
-            'entered_at': '11-04-2018 25:34',
-            'duration': '25:03',
-        }
-    ]
+    non_closed_visits = []
+
+    for visit in unclosed_visits:
+        non_closed_visits.append(
+            {
+                'who_entered': visit.passcard.owner_name,
+                'entered_at': localtime(visit.entered_at, get_default_timezone()),
+                'duration': visit.formatted_duration(),
+            })
+
     context = {
-        'non_closed_visits': non_closed_visits,  # не закрытые посещения
+        'non_closed_visits': non_closed_visits,
     }
     return render(request, 'storage_information.html', context)
